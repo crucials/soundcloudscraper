0b83e0f63cbc381dd645ee4e9e38230f0245ace5
crucials <ensueness@gmail.com>
2024-06-25 18:49:57 +0500

    implemented recursive followers scraping

--- main.py
+++ main.py
@@ -29,7 +29,16 @@ def userid(artist, id):
     return data["id"]
 
 
-def getfollowerdata(userid, id):
+def getfollowersdata(userid,
+                     id,
+                     max_followers_depth: int,
+                     current_followers_depth: int = 0,
+                     username=None):
+    if current_followers_depth > max_followers_depth:
+        return
+
+    print(f'getting followers of {username or userid}')
+
     url = f"https://api-v2.soundcloud.com/users/{userid}/followers?client_id={id}&limit={limit}&offset=0&linked_partitioning=1"
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
@@ -37,6 +46,8 @@ def getfollowerdata(userid, id):
         with open("export.txt", "a", encoding="utf-8") as file:
             for follower in data["collection"]:
                 followerid = follower["urn"].split(":")[2]
+                follower_username = follower['username']
+                print(f'follower of {userid}: {follower_username}')
                 tracksurl = f"https://api-v2.soundcloud.com/users/{followerid}/tracks?limit={limit}&client_id={id}"
                 tracksrequest = urllib.request.Request(tracksurl)
                 with urllib.request.urlopen(tracksrequest) as tracks_response:
@@ -45,11 +56,27 @@ def getfollowerdata(userid, id):
                         for track in tracksdata["collection"]:
                             file.write(str(track))
                             file.write("\n")
+                
+                getfollowersdata(followerid, id, max_followers_depth,
+                                 current_followers_depth + 1, username=follower['username'])
 
 
 id = clientid()
 print(id)
 artist = input("artist url: \n")
-userid = userid(artist, id)
-print(userid)
-getfollowerdata(userid, id)
+
+try:
+    followers_collecting_depth = input('\nhow deep to collect followers (1 - get followers '
+                                       +'of followers, 2 - followers of followers of '
+                                       + 'followers and so on) \npress enter to skip '
+                                       + 'and collect only followers of target user:\n')
+    if len(followers_collecting_depth.strip()) == 0:
+        followers_collecting_depth = 0
+    else:
+        followers_collecting_depth = int(followers_collecting_depth)
+    
+    userid = userid(artist, id)
+    print(userid)
+    getfollowersdata(userid, id, int(followers_collecting_depth), username=artist)
+except ValueError as error:
+    print('entered value must be a number')
